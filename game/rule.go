62c72daecd01c0be595aa5cd69eae289aed6c7e6
KennHuang <kennhung@users.noreply.github.com>
2022-07-14 09:05:36 +0800

    [modify] Update 2022 camp rules

--- game/rule.go
+++ game/rule.go
@@ -15,21 +15,13 @@ type Rule struct {
 
 // All rules from the 2022 game that carry point penalties.
 var rules = []*Rule{
-	// {1, "G103", false, false, "BUMPERS must be in Bumper Zone (see R402) during the match."},
-	{1, "G15", true, false, "不得進入 Collection 超過5秒"},
-	{2, "O1", true, false, "友善溝通，禁止言語怒罵，例: 問候、歧視、嘲諷等"},
-	{3, "O2", true, false, "Driver Station內限制最多三人(driver, driver, coach)"},
-	{4, "G22", true, false, "機器人上 bumper 不能分離機器人"},
-	{5, "G23", true, false, "不能將 bumper 舉起來"},
-	{6, "G21", true, false, "機器人不能分離"},
-	{7, "G18", true, false, "不得接觸敵方機器人超過5秒"},
-	{8, "G17", true, false, "機器人倒下要遠離他"},
-	{9, "G19", true, false, "不得刻意傷害、連接他人機器人"},
-	{10, "H7", true, false, "只能使用 Catching 將物件丟進場地內"},
-	{11, "O3", true, false, "比賽結果由裁判評分，請尊重裁判判決"},
-	{12, "O4", false, false, "每台機器人至多攜帶3個場地物件,超過五秒視為犯規"},
-	{13, "O5", true, false, "機器人或機器人上的物件觸碰到 Trap"},
-	{14, "O6", true, false, "機器人或機器人上的物件在非 Endgame 時觸碰到 Cube"},
+	{1, "C1", true, false, "不得進入自己的 Collection"},
+	{2, "C6", false, false, "機器人不能分離"},
+	{3, "C6", true, false, "機器人不能分離"},
+	{4, "C7", false, false, "不得接觸敵方機器人超過5秒"},
+	{5, "C10", true, false, "只能使用 Catching 將物件丟進場地內"},
+	{6, "C11", false, false, "每台機器人至多控制3個場地物件，超過五秒視為犯規"},
+	{7, "C12", true, false, "機器人或機器人控制的物件觸碰到Trap或是在endgame時間外碰到cube"},
 }
 var ruleMap map[int]*Rule
 
